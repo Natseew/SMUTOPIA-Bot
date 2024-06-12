05898f86455f8f4f888e96457083b034551ce880
natseew <natseew@gmail.com>
2024-06-13 00:24:18 +0800

    initialise clan routes

--- initDb.py
+++ initDb.py
@@ -6,9 +6,16 @@ cred = credentials.Certificate("smu-fo2024-smutopiabot-firebase-adminsdk-tt5im-6
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-doc_ref = db.collection("books")
+doc_ref = db.collection("hunt").document("clan_info")
 
-with open("book_info.json", "r") as f:
+with open("sub_clan_info.json", "r") as f:
 	file_contents = json.load(f)
-doc_ref.set(file_contents)
+	doc_ref.set(file_contents)
+
+doc = doc_ref.get()
+if doc.exists:
+	dic = doc.to_dict()
+	print(f"Document data: {dic}")
+else:
+    print("No such document!")
 
--- scav.py
+++ scav.py
@@ -4,9 +4,25 @@ from typing import Final
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
+import firebase_admin
+from firebase_admin import credentials, db, firestore
+import json
+
+#initialize firebase
+cred = credentials.Certificate("smu-fo2024-smutopiabot-firebase-adminsdk-tt5im-6f3313e2ae.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+doc_ref = db.collection("hunt").document("clan_info")
+
+#initialize route_info
+with open("route_info.json", "r") as route:
+	route_contents = json.load(route)
+
 #global Variables
 hunt_started = False
 hunt_stopped = False
+sub_clan = ""
 BOT_USERNAME : Final = "@SMUTOPIABOT"
 
 def startTimer(waitForSeconds):
@@ -29,42 +45,53 @@ async def stop_hunt():
 async def scav_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global hunt_started
     hunt_started = True
-    await update.message.reply_text('What is your group number?')
+    await update.message.reply_text('What is your group name?')
     startTimer(20)
-    
-    
+
+#functions
+def initialise_clan(clan: str) -> str:
+  doc = doc_ref.get()
+  if doc.exists:
+    dic = doc.to_dict()
+    if dic[clan]["progress"] == ['start']:
+      dic[clan]["progress"] = route_contents[dic[clan]["route"]]["route"]
+      doc_ref.set(dic)
+      return "Please Key to start the scavenger hunt and get your first clue."
+    else:
+      return 'To continue hunt please key in the next password for the next clue.'
+  else:
+    return 'Unable to retrieve data.'
 
 #Responses
 def handle_response_scav(text: str) -> str:
-    if '1' in text:
-        return 'Your first clue is .....'
-    if '2' in text:
-        return 'Your first clue is .....'
-    if text == 'SCIS':
-        return 'Your second clue is'
-
+  try:
+    if text.upper() in ["AZU_1","XOLO_1"]:
+      return initialise_clan(text.upper())
+    else:
+      return 'Pass'
+  except:
     return 'I do not understand your message.'
 
 async def handle_message(update:Update, context: ContextTypes.DEFAULT_TYPE):
     global hunt_stopped
-    
+  
     message_type: str = update.message.chat.type
     text: str = update.message.text
 
     print(f'User ({update.message.chat.id}),({update.message.chat.username}) in {message_type}: "{text}"')
     if hunt_started:
-        if 'group' in message_type:
-            if BOT_USERNAME in text:
-                new_text: str = text.replace(BOT_USERNAME, '').strip()
-                response: str = handle_response_scav(new_text)
-            else:
-                return  
-        else:
-            response: str = handle_response_scav(text)
+      if 'group' in message_type:
+          if BOT_USERNAME in text:
+              new_text: str = text.replace(BOT_USERNAME, '').strip()
+              response: str = handle_response_scav(new_text)
+          else:
+              return  
+      else:
+          response: str = handle_response_scav(text)
 
-        print('Bot:', response)
-        
-        await update.message.reply_text(response)
+      print('Bot:', response)
+      
+      await update.message.reply_text(response)
   
     if hunt_stopped:
       if 'group' in message_type:
