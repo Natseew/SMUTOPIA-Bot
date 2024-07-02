d0eb420f79352fcacafd17f7e8590eb194c44f24
natseew <natseew@gmail.com>
2024-07-02 22:40:08 +0800

    added campus map

--- lunch.py
+++ lunch.py
@@ -1,27 +1,39 @@
 from typing import Final
-from telegram import Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+
+import json
 
 BOT_USERNAME : Final = "@SMUTOPIABOT"
 
+#initialize clue_info
+with open("lunch_info.json", "r") as menu:
+	menu_contents = json.load(menu)
+
 #lunch starting command
 async def lunch_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  message_type: str = update.message.chat.type
-  text: str = update.message.text
-  if 'group' in message_type:
-    if BOT_USERNAME in text:
-        new_text: str = text.replace(BOT_USERNAME, '').strip()
-        response: str = handle_response_lunch(new_text)
-    else:
-        return  
-  else:
-      response: str = handle_response_lunch(text)
+  keyboard = [
+    [
+        InlineKeyboardButton("Day 1", callback_data="D1"),
+        InlineKeyboardButton("Day 2", callback_data="D2"),
+    ],
+    [
+      InlineKeyboardButton("Day 3", callback_data="D3")
+    ],
+  ]
 
-  print('Bot:', response)
-  
-  await update.message.reply_text(response)
+  reply_markup = InlineKeyboardMarkup(keyboard)
+
+  await update.message.reply_text("Please choose:", reply_markup=reply_markup)
 
 #Responses
-def handle_response_lunch(text: str) -> str:
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+  """Parses the CallbackQuery and updates the message text."""
+  query = update.callback_query
 
-    return 'My Boy Cooked'
+  # CallbackQueries need to be answered, even if no notification to the user is needed
+  # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+  await query.answer()
+
+  await query.edit_message_text(text=f"Selected option: " + menu_contents[{query.data}])
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from telegram.ext import *
 
 
 from scav import handle_message, scav_command
-from lunch import lunch_command
+from lunch import lunch_command, button
 
 
 load_dotenv()
@@ -18,7 +18,9 @@ BOT_USERNAME : Final = "@SMUTOPIABOT"
 
 #Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.message.chat_id
     await update.message.reply_text("Welcome to SMUtopia, a realm where nature and magic entwine around the majestic World Tree. But peril looms – the Tree fades, poisoned by darkness. You, esteemed adventurers, are our chosen champions, embarking on the legendary Heroes Quest. Guided by wise Mentors (Facis) and challenged by Gatekeepers (Game Masters), your journey will test your courage and forge friendships. Embrace the adventure, uphold our CIRCLE values, and save our beloved land. Are you ready for the challenge?")
+    await context.bot.send_photo(chat_id=chat_id, photo="./images/campus-map.jpeg")
     await update.message.reply_text("Click on /help to get instructions.")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -27,14 +29,14 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     In this quest, you are tasked with a series of challenges located within different parts of the SMU Campus only. As you attempt to successfully complete these challenges, you will be rewarded with potions (points).
     
-    Any failure to complete any challenges will just result in no potions being awarded. The fastest subclan to finish this quest will be awarded with additional 100 potions. 
+    Any failure to complete any challenges will just result in no potions being awarded. The fastest subclan to finish this quest will be awarded with additional potions. 
     
     Here’s how your quest will unfold:
     -   You will be on your own, as a subclan during this quest (No Mentors).
 
     -   The same phone have to be used to communicate with the bot throughout the quest.
 
-    -   Duration: You have a maximum of 1.5 hours (until 11.45am) to complete the quest, the faster the better.
+    -   Duration: You have a maximum of 2 hours 30 minutes to complete the quest, the faster the better.
 
     -   Guidance: Each SC will use the clue given at every station by the gatekeepers to reveal the clue to their next destination via this bot. Use the provided school map and photo references to verify each location.
 
@@ -44,7 +46,7 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     -   Lib Quest: you only need to visit your assigned library, LKS or KGC, not both to complete a challenge as part of your quest.
     
-    -   SSH/Cozy Haven: This is just a touch and go station.
+    -   COmpletion: You will have completed the quest once the Bot congratulates you on your completion. Upon completion contact your facilitators who will bring you to your next location.
 
     -   Please send your message with (@SMUTOPIABOT) when submitting answers.
     
@@ -69,6 +71,7 @@ if __name__ == '__main__':
 
     #Messages
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    app.add_handler(CallbackQueryHandler(button))
 
     #Errors
     app.add_error_handler(error)
